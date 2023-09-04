60ce77b8303add3323bfb3907e7c08f335f0079d
Pablo Llanes <pablodavidllanes@gmail.com>
2023-09-04 11:34:20 -0500

    Finish redux module

---
+++ redux-app-6/src/components/Cart/CartButton.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
+import classes from './CartButton.module.css';
+
+const CartButton = (props) => {
+    const dispatch = useDispatch();
+    const totalItems = useSelector(state => state.cart.totalQuantity);
+
+    const toggleCartHandler = () => {
+        console.log('hello');
+        dispatch(uiActions.toggle());
+    };
+
+    return (
+        <button className={classes.button} onClick={toggleCartHandler}>
+            <span>My Cart</span>
+            <span className={classes.badge}>{totalItems}</span>
+        </button>
+    );
+};
+
+export default CartButton;
